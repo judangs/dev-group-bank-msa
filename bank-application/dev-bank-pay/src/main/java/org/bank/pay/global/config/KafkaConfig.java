cbd02051eab8e24f0c5fa471cdf999a868d780de
judangs <wneotjd228@naver.com>
2025-01-27 03:12:48 +0900

    feat: 카프카 프로듀서 설정 추가

---
+++ bank-application/dev-bank-pay/src/main/java/org/bank/pay/global/config/KafkaConfig.java
@@ -0,0 +1,44 @@
+package org.bank.pay.global.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.bank.core.kafka.KafkaEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@PropertySource("classpath:application-kafka-producer.properties")
+@EnableConfigurationProperties(KafkaProperties.class)
+public class KafkaConfig {
+
+    @Autowired
+    private KafkaProperties kafkaProperties;
+
+    @Bean
+    public ProducerFactory<String, KafkaEvent> producerFactory() {
+
+        KafkaProperties.Producer producerProperties = kafkaProperties.getProducer();
+
+        Map<String, Object> factoryProperties = new HashMap<>();
+        factoryProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, producerProperties.getBootstrapServers().get(0));
+        factoryProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, producerProperties.getKeySerializer());
+        factoryProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, producerProperties.getValueSerializer());
+        factoryProperties.put(ProducerConfig.ACKS_CONFIG, producerProperties.getAcks());
+
+        return new DefaultKafkaProducerFactory<>(factoryProperties);
+    }
+
+    @Bean
+    public KafkaTemplate<String, KafkaEvent> kafkaTemplate(ProducerFactory<String, KafkaEvent> producerFactory) {
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
---
+++ bank-application/dev-bank-pay/src/main/java/org/bank/pay/global/kafka/KafkaEventSerializer.java
@@ -0,0 +1,42 @@
+package org.bank.pay.global.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+import org.bank.core.kafka.KafkaEvent;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.util.Map;
+
+public class KafkaEventSerializer implements Serializer<KafkaEvent> {
+
+    private final ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json()
+            .modulesToInstall(new JavaTimeModule())
+            .build();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        Serializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public byte[] serialize(String s, KafkaEvent kafkaEvent) {
+        try {
+            return objectMapper.writeValueAsBytes(kafkaEvent);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, KafkaEvent data) {
+        return Serializer.super.serialize(topic, headers, data);
+    }
+
+    @Override
+    public void close() {
+        Serializer.super.close();
+    }
+}
