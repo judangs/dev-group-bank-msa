9493ff8d18037766b7b0788314f4d2b32015c6b9
judangs <wneotjd228@naver.com>
2025-01-28 04:30:27 +0900

    test: family 비즈니스 로직 테스트

--- bank-domain/dev-bank-pay/src/test/java/org/bank/pay/core/familly/FamilyTest.java
+++ bank-domain/dev-bank-pay/src/test/java/org/bank/pay/core/familly/FamilyTest.java
@@ -3,53 +3,20 @@ package org.bank.pay.core.familly;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class FamilyTest {
 
-    private Family family = new Family();
-    private MemberClaims leader = new MemberClaims("fixture-id", "fixture", "fixture@bank.com");
+    private final MemberClaims leader = new MemberClaims("fixture-id", "fixture", "fixture@bank.com");
 
     @Test
-    @DisplayName("패밀리를 생성합니다.")
-    void createFamilly() {
-
-        family.createFamilly(leader);
+    @DisplayName("family를 생성합니다")
+    void family를_생성합니다() {
+        Family family = new Family(leader);
 
         assertAll(
                 () -> assertEquals(leader, family.getLeader()),
                 () -> assertFalse(family.getParticipants().isEmpty())
         );
     }
-
-    @Test
-    @DisplayName("패밀리 멤버를 추가합니다.")
-    void addMember() {
-        MemberClaims newMeber = new MemberClaims("new-fuxture", "new", "new@bank.com");
-
-        int participants = family.getParticipants().size();
-        family.addMember(newMeber);
-
-        assertEquals(family.getParticipants().size(), participants + 1);
-    }
-
-    @Test
-    @DisplayName("패밀리 리더를 변경합니다.")
-    void changeLeader() {
-
-        MemberClaims newMeber = new MemberClaims("new-fuxture", "new", "new@bank.com");
-
-        assertAll(
-                () -> assertThrows(IllegalArgumentException.class, () -> family.changeLeader(newMeber)),
-                () -> {
-                    family.addMember(newMeber);
-                    family.changeLeader(newMeber);
-                    assertEquals(family.getLeader(), newMeber);
-                }
-        );
-    }
-
 }
