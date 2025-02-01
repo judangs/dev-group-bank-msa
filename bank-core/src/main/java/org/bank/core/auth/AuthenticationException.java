b0fcf985ad8745bca541dd0f2e731535fe53129b
judangs <wneotjd228@naver.com>
2025-02-01 20:34:28 +0900

    refactor: 인증과 예외의 생성자 오버로딩
    Fixes Issue: #1

--- bank-core/src/main/java/org/bank/core/auth/AuthenticationException.java
+++ bank-core/src/main/java/org/bank/core/auth/AuthenticationException.java
@@ -2,6 +2,10 @@ package org.bank.core.auth;
 
 public class AuthenticationException extends RuntimeException {
 
+    public AuthenticationException() {
+        super();
+    }
+
     public AuthenticationException(String message) {
         super(message);
     }
