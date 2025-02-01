dd457216d7a897a7d1a9372ee437da897d6a29dd
judangs <wneotjd228@naver.com>
2025-02-01 20:06:17 +0900

    test: 도메인 오브젝트가 제공해야 할 서비스들의 단위 테스트
    Fixes Issue: #1

---
+++ bank-domain/techfin-user/src/test/java/org/bank/user/core/domain/account/service/CredentialServiceTest.java
@@ -0,0 +1,52 @@
+package org.bank.user.core.domain.account.service;
+
+import org.assertj.core.api.Assertions;
+import org.bank.user.core.domain.account.Credential;
+import org.bank.user.core.domain.account.repository.CredentialRepository;
+import org.bank.user.core.domain.account.repository.ProfileRepository;
+import org.bank.user.core.domain.fixture.AccountFixture;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = CredentialServiceTest.UnitTest.class)
+class CredentialServiceTest {
+
+    @Configuration
+    @TestPropertySource(locations = "classpath:application.properties")
+    @ComponentScan(basePackages = "org.bank.user.core.domain.account")
+    static class UnitTest {
+
+    }
+
+    @Configuration
+    static class StubPool {
+        @MockBean
+        private CredentialRepository credentialRepository;
+        @MockBean
+        private ProfileRepository profileRepository;
+    }
+
+    @Autowired
+    private CredentialService credentialService;
+
+    @Test
+    @DisplayName("새로운 임시 패스워드를 생성합니다")
+    void 새로운_임시_패스워드를_생성합니다() {
+
+        Credential credential  = AccountFixture.authenticated("user-01");
+
+        String origin = credential.getPassword();
+        String temporal = credentialService.createTemporalPassword(credential);
+
+        Assertions.assertThat(temporal).isNotEqualTo(origin);
+    }
+}
---
+++ bank-domain/techfin-user/src/test/java/org/bank/user/core/domain/auth/service/SessionRenewalServiceTest.java
@@ -0,0 +1,58 @@
+package org.bank.user.core.domain.auth.service;
+
+import org.bank.core.auth.AuthConstants;
+import org.bank.user.core.domain.account.repository.CredentialRepository;
+import org.bank.user.core.domain.auth.TokenContents;
+import org.bank.user.core.domain.auth.repository.TokenRedisRepository;
+import org.bank.user.core.domain.fixture.TokenContentsFixture;
+import org.bank.user.core.domain.jwt.service.JwtProvider;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+class SessionRenewalServiceTest {
+
+    @Configuration
+    @ComponentScan(basePackages = {
+            "org.bank.user.core.domain.auth",
+            "org.bank.user.core.domain.jwt"
+    })
+    static class UnitTest { }
+
+    @Autowired
+    private JwtProvider jwtProvider;
+    @Autowired
+    private SessionRenewalService sessionRenewalService;
+    @MockBean
+    private TokenRedisRepository tokenRedisRepository;
+    @MockBean
+    private CredentialRepository credentialRepository;
+
+
+    @Test
+    @DisplayName("액세스 토큰을 발급받아 리프레시 토큰의 유효성을 검사하고 액세스 토큰을 재발행합니다")
+    void 액세스_토큰을_발급받아_리프레시_토큰의_유효성을_검사하고_액세스_토큰을_재발행합니다() {
+
+        TokenContents tokenContents = TokenContentsFixture.fixture();
+        String refresh = jwtProvider.generate(tokenContents, AuthConstants.TokenType.REFRESH);
+
+        when(tokenRedisRepository.findByToken(any())).thenReturn(Optional.ofNullable(refresh));
+        String access = sessionRenewalService.validateAndRenewTokenIfNotExpired("");
+        assertThat(access).isNotNull();
+    }
+
+}
