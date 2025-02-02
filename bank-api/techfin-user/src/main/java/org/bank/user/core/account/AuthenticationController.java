cda9d7e145dcc6583ced230854a981dfa6da15c6
judangs <wneotjd228@naver.com>
2025-02-03 02:12:39 +0900

    refactor: 인증 엔드포인트 작성
    사용자 로그인 / 로그아웃 / 토큰 재발급과 같은 기능
    
    Related: #1

---
+++ bank-api/techfin-user/src/main/java/org/bank/user/core/account/AuthenticationController.java
@@ -0,0 +1,54 @@
+package org.bank.user.core.account;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.bank.core.auth.AuthClaims;
+import org.bank.core.auth.AuthenticatedUser;
+import org.bank.core.dto.response.ResponseCodeV2;
+import org.bank.core.dto.response.ResponseDtoV2;
+import org.bank.user.core.facade.account.UserAccountAuthenticationFacade;
+import org.bank.user.dto.service.request.LoginRequest;
+import org.bank.user.dto.service.response.LoginResponse;
+import org.bank.user.global.resolver.AuthorizationHeaderResolver;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user/auth")
+public class AuthenticationController {
+
+    private final AuthorizationHeaderResolver authorizationHeaderResolver;
+    private final UserAccountAuthenticationFacade userAccountAuthenticationFacade;
+
+    @PostMapping("/login")
+    public ResponseEntity<? super LoginResponse> login(@RequestBody LoginRequest request, HttpServletResponse httpServletResponse) {
+
+        ResponseDtoV2 loginResponse = userAccountAuthenticationFacade.login(request.getUserid(), request.getPassword());
+        if(loginResponse instanceof LoginResponse response) {
+            if(loginResponse.getCode().equals(ResponseCodeV2.SUCCESS)) {
+                authorizationHeaderResolver.addTokenToResponseHeader(httpServletResponse, response.getToken().getAccess());
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<? super LoginResponse> logout(
+            @AuthenticatedUser AuthClaims user,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorization
+    ) {
+
+        String token = authorizationHeaderResolver.parseRequestHeaderToToken(authorization);
+        userAccountAuthenticationFacade.logout(user, token);
+
+        ResponseDtoV2 response = ResponseDtoV2.success("로그아웃에 성공했습니다.");
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+}
+
