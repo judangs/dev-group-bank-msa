be76409d495ca8282ef6e8e1bd521566ec4f0cc0
judangs <wneotjd228@naver.com>
2025-02-12 10:27:44 +0900

    refactor: 프로젝트 범위가 가지는 도메인의 이름에 대해 enum으로 변경

--- bank-core/src/main/java/org/bank/core/domain/DomainNames.java
+++ bank-core/src/main/java/org/bank/core/domain/DomainNames.java
@@ -1,7 +1,5 @@
 package org.bank.core.domain;
 
-public class DomainNames {
-
-    public static final String USER = "user";
-    public static final String PAY = "pay";
+public enum DomainNames {
+    USER, PAY, NONE;
 }
