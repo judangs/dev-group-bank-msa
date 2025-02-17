414c8f95aae2398ecf5c16225b169465193f855f
judangs <wneotjd228@naver.com>
2025-02-17 15:28:05 +0900

    feat: 게이트웨이 애플리케이션
    Related to: #12

--- bank-cloud/techfin-gateway/src/main/java/org/bank/gateway/GatewayApplication.java
+++ bank-cloud/techfin-gateway/src/main/java/org/bank/gateway/GatewayApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevGatewayApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(DevGatewayApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
