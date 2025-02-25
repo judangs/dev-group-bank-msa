aea7c102a2fea029aecd1b49d20d4eca3032bc42
judangs <wneotjd228@naver.com>
2025-02-25 23:35:05 +0900

    refactor: history 오브젝트의 불필요한 필드를 삭제하고 코드를 개선
    불필요한 필드를 정리
    
    Related to: #23

--- bank-domain/dev-bank-pay/src/main/java/org/bank/pay/core/domain/history/OrderPayHistory.java
+++ bank-domain/dev-bank-pay/src/main/java/org/bank/pay/core/domain/history/OrderPayHistory.java
@@ -4,14 +4,34 @@ import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.bank.core.auth.AuthClaims;
+import org.bank.core.cash.Money;
+import org.bank.core.cash.PayMethod;
+import org.bank.core.payment.Product;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @DiscriminatorValue("order")
+@SuperBuilder
 @Entity
 public class OrderPayHistory extends PayHistory {
 
-    private String orderId;
     private String storeName;
-    private String username;
+
+
+    public static OrderPayHistory of(AuthClaims user, String paymentId, Product product) {
+        return OrderPayHistory.builder()
+                .userId(user.getUserid())
+                .paymentId(paymentId)
+                .payName(product.getName())
+                .method(PayMethod.CARD)
+                .payMoney(new Money(product.price()))
+                .transactionDate(LocalDateTime.now())
+                .rollbackDate(LocalDate.of(9999, 12, 31).atTime(23, 59))
+                .build();
+    }
 }
