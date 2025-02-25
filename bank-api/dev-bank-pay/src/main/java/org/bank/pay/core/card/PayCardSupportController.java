b6cbe318de0ed11de8c05e311035af1eeafd2c73
judangs <wneotjd228@naver.com>
2025-02-25 23:46:16 +0900

    refactor: 페이 api 모듈의 설정 및 코드 리팩토링
    swagger 추가, 인증 헤더 관련 커스텀 어노테이션 추가, Response 버저닝 적용
    
    Related to: #23

---
+++ bank-api/dev-bank-pay/src/main/java/org/bank/pay/core/card/PayCardSupportController.java
@@ -0,0 +1,54 @@
+package org.bank.pay.core.card;
+
+import lombok.RequiredArgsConstructor;
+import org.bank.core.auth.AuthClaims;
+import org.bank.core.auth.AuthenticatedUser;
+import org.bank.core.dto.response.ResponseDtoV2;
+import org.bank.pay.dto.service.request.CardPaymentRequest;
+import org.bank.pay.dto.service.request.UpdateCardAliasRequest;
+import org.bank.pay.global.http.HttpResponseEntityStatusConverter;
+import org.bank.pay.global.swagger.spec.PayCardSupportSwaggerSpec;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/pay/card")
+@RequiredArgsConstructor
+public class PayCardSupportController implements PayCardSupportSwaggerSpec {
+
+    private final HttpResponseEntityStatusConverter httpResponseEntityStatusConverter;
+
+    private final CardFacade cardFacade;
+
+    @GetMapping
+    public ResponseEntity<? extends ResponseDtoV2> card(@AuthenticatedUser AuthClaims user) {
+        ResponseDtoV2 response = cardFacade.getRegisteredCards(user);
+        return httpResponseEntityStatusConverter.convert(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<? extends ResponseDtoV2> register(@AuthenticatedUser AuthClaims user,
+                                                        CardPaymentRequest request) {
+        ResponseDtoV2 response = cardFacade.registerCard(user, request);
+        return httpResponseEntityStatusConverter.convert(response);
+    }
+
+    @PutMapping("{cardId}/alias")
+    public ResponseEntity<? extends ResponseDtoV2> alias(@AuthenticatedUser AuthClaims user,
+                                                     @PathVariable UUID cardId,
+                                                     UpdateCardAliasRequest request) {
+        ResponseDtoV2 response = cardFacade.updateCardAlias(user, cardId, request.getCardName());
+        return httpResponseEntityStatusConverter.convert(response);
+    }
+
+    @DeleteMapping("/{cardId}")
+    public ResponseEntity<? extends ResponseDtoV2> delete(@AuthenticatedUser AuthClaims user,
+                                                      @PathVariable UUID cardId) {
+        cardFacade.deleteRegisteredCard(user, cardId);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
