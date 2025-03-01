cc252f09c25deef43a891dd67ef0d1cd19052743
judangs <wneotjd228@naver.com>
2025-03-01 20:15:45 +0900

    refactor: family 레포지토리 인터페이스 명세 변경
    Related to: #34

--- bank-domain/dev-bank-pay/src/main/java/org/bank/pay/core/domain/familly/repository/FamilyEventStore.java
+++ bank-domain/dev-bank-pay/src/main/java/org/bank/pay/core/domain/familly/repository/FamilyEventStore.java
@@ -1,9 +1,8 @@
 package org.bank.pay.core.domain.familly.repository;
 
-import org.bank.pay.core.event.family.FamilyInvitation;
-import org.bank.pay.core.event.family.FamilyPayment;
+import org.bank.pay.core.event.family.FamilyEventEntity;
 
 public interface FamilyEventStore {
-    void store(FamilyInvitation invitation);
-    void store(FamilyPayment paymentRequest);
+
+    void store(FamilyEventEntity event);
 }
