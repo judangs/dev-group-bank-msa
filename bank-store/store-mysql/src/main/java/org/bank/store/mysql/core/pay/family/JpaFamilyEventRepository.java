92617454e8773c4c355445b24ed2283782ab5d5b
judangs <wneotjd228@naver.com>
2025-03-01 20:25:25 +0900

    test: 패밀리 그룹에서 발생하는 이벤트를 저장하기 위한 jpa 레포지토리 생성
    Related to: #34

---
+++ bank-store/store-mysql/src/main/java/org/bank/store/mysql/core/pay/family/JpaFamilyEventRepository.java
@@ -0,0 +1,25 @@
+package org.bank.store.mysql.core.pay.family;
+
+import org.bank.core.auth.AuthClaims;
+import org.bank.pay.core.event.family.FamilyEventEntity;
+import org.bank.pay.core.event.family.FamilyInvitation;
+import org.bank.pay.core.event.family.FamilyPayment;
+import org.bank.store.mysql.global.jpa.JpaBaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface JpaFamilyEventRepository extends JpaBaseRepository<FamilyEventEntity, UUID> {
+
+    @Query("SELECT fi FROM FamilyInvitation fi WHERE fi.to = :user")
+    Optional<FamilyInvitation> findInvitationEventByUser(@Param("user") AuthClaims user);
+
+    @Query("SELECT fi FROM FamilyInvitation fi WHERE fi.id = :eventId")
+    Optional<FamilyInvitation> findInvitationEventById(@Param("eventId") UUID eventId);
+
+    @Query("SELECT fp FROM FamilyPayment fp WHERE fp.id = :eventId AND fp.familyId = :familyId")
+    Optional<FamilyPayment> findPaymentRequestEventByFamilyIdAndEventId(@Param("familyId") UUID familyId, @Param("eventId") UUID eventId);
+
+}
