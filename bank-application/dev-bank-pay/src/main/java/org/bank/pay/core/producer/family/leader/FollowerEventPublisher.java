0994355bd51e6d5cab971a16f6f48c56948eb86f
judangs <wneotjd228@naver.com>
2025-03-04 20:18:53 +0900

    feat: 카프카 이벤트 발행을 위한 publisher 오브젝트 추가
    Related to: #32

---
+++ bank-application/dev-bank-pay/src/main/java/org/bank/pay/core/producer/family/leader/FollowerEventPublisher.java
@@ -0,0 +1,25 @@
+package org.bank.pay.core.producer.family.leader;
+
+import lombok.RequiredArgsConstructor;
+import org.bank.core.auth.AuthClaims;
+import org.bank.core.kafka.KafkaEvent;
+import org.bank.pay.core.domain.familly.Family;
+import org.bank.pay.core.domain.familly.MemberClaims;
+import org.bank.pay.core.event.family.kafka.InviteEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FollowerEventPublisher {
+
+    private final String INVITATION_TOPIC = "family.invitation";
+
+    private final KafkaTemplate<String, KafkaEvent> kafkaTemplate;
+
+    @Async
+    public void invite(Family family, AuthClaims follower) {
+        kafkaTemplate.send(INVITATION_TOPIC, new InviteEvent(family.getFamilyId(), MemberClaims.of(follower)));
+    }
+}
