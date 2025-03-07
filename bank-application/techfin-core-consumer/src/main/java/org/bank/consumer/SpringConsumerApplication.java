95286a83cd5329d172cfae885474f1fac87f6ba3
judangs <wneotjd228@naver.com>
2025-03-07 15:34:48 +0900

    refactor: 패밀리 결제 이벤트를 처리하는 인터페이스 활용에 따라 기존의 오브젝트 네이밍 변경
    인터페이스를 적용하고 기본으로 사용되는 구현체와 별도로 오브젝트의 역할을 명시할 수 있도록 네이밍
    
    related to: #37

--- bank-application/techfin-core-consumer/src/main/java/org/bank/consumer/SpringConsumerApplication.java
+++ bank-application/techfin-core-consumer/src/main/java/org/bank/consumer/SpringConsumerApplication.java
@@ -2,8 +2,16 @@ package org.bank.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(scanBasePackages = "org.bank")
+@SpringBootApplication(
+        scanBasePackages = {
+                "org.bank"
+        },
+        exclude = {
+                DataSourceAutoConfiguration.class
+        }
+)
 public class SpringConsumerApplication {
 
     public static void main(String[] args) {
